5cc41c36ab90280928d8f52494c220fa6cc45082
kristinaHome <nielsenlabmbi@gmail.com>
2020-07-09 18:06:39 -0400

    fix2

--- modules/configPstate_RC2Gratings.m
+++ modules/configPstate_RC2Gratings.m
@@ -15,22 +15,24 @@ Pstate.param{7} = {'y_size'      'float'      10       1                'deg'};
 Pstate.param{8} = {'mask_type'   'string'   'none'       1                ''};
 Pstate.param{9} = {'mask_radius' 'float'      6       0                'deg'};
 
-Pstate.param{10} = {'min_ori'         'int'        0       0                'deg'};
-Pstate.param{11} = {'orirange'         'int'        360       0                'deg'};
-Pstate.param{12} = {'n_ori'    'int'   12       0                ''};
+Pstate.param{10} = {'contrast'    'float'     50       0                '%'}; 
 
-Pstate.param{13} = {'n_phase'   'int'   4       0                ''};
+Pstate.param{11} = {'min_ori'         'int'        0       0                'deg'};
+Pstate.param{12} = {'orirange'         'int'        360       0                'deg'};
+Pstate.param{13} = {'n_ori'    'int'   12       0                ''};
 
-Pstate.param{14} = {'h_per'      'int'   20       1               'frames'};
-Pstate.param{15} = {'blankProb'    'float'   0.05       0                ''};
+Pstate.param{14} = {'n_phase'   'int'   4       0                ''};
 
-Pstate.param{16} = {'s_freq'   'float'   0.125       1                'c/deg'};
-Pstate.param{17} = {'s_profile'   'string'   'sin'       1                ''};
-Pstate.param{18} = {'s_duty'      'float'   0.5       0                ''};
-Pstate.param{19} = {'drift'    'int'   1         0                 'binary'};
-Pstate.param{20} = {'t_period'    'int'       20       1                'frames'};
+Pstate.param{15} = {'h_per'      'int'   20       1               'frames'};
+Pstate.param{16} = {'blankProb'    'float'   0.05       0                ''};
 
-Pstate.param{21} = {'rseed'    'int'   1       0                ''};
+Pstate.param{17} = {'s_freq'   'float'   0.125       1                'c/deg'};
+Pstate.param{18} = {'s_profile'   'string'   'sin'       1                ''};
+Pstate.param{19} = {'s_duty'      'float'   0.5       0                ''};
+Pstate.param{20} = {'drift'    'int'   1         0                 'binary'};
+Pstate.param{21} = {'t_period'    'int'       20       1                'frames'};
 
-Pstate.param{22} = {'Leye_bit'    'int'   1       0                ''};
-Pstate.param{23} = {'Reye_bit'    'int'   1       0                ''};
+Pstate.param{22} = {'rseed'    'int'   1       0                ''};
+
+Pstate.param{23} = {'Leye_bit'    'int'   1       0                ''};
+Pstate.param{24} = {'Reye_bit'    'int'   1       0                ''};
