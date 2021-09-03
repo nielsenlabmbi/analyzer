db5f0e80d2172279b2a1413f951b7fce17ca182b
nielsenlab <nielsenlabmbi@gmail.com>
2021-09-03 14:40:02 -0400

    zaber rotation

---
+++ modules/configPstate_ZaberRot.m
@@ -0,0 +1,22 @@
+function configPstate_ZaberRot
+
+%periodic grater
+
+global Pstate Pdoc
+
+Pstate = struct; %clear it
+Pdoc=struct;
+
+Pdoc.general='zaber stages without vis stim';
+
+Pstate.param{1} = {'predelay'  'float'      1       0                'sec'};
+Pdoc.param{1}='Predelay duration';
+Pstate.param{end+1} = {'postdelay'  'float'    1       0                'sec'};
+Pdoc.param{end+1}='Postdelay duration';
+Pstate.param{end+1} = {'rotDur'  'float'     0.5       0                'sec'};
+Pdoc.param{end+1}='Rotation duration';
+
+Pstate.param{end+1}={'rotSpeed'     'float'    70  1   'deg/s'};
+Pdoc.param{end+1}='Rotation speed';
+
+
