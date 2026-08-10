7ac85d94946538796d9601639d60a97f93e5ec7f
Kristina <nielsenlabmbi@gmail.com>
2026-08-10 16:37:11 -0400

    opto

--- modules/configPstate_Opto.m
+++ modules/configPstate_Opto.m
@@ -6,13 +6,14 @@ global Pstate
 Pstate = struct; %clear it
 
 Pstate.param{1} = {'predelay'  'float'      2       0                'sec'};
-Pstate.param{2} = {'postdelay'  'float'     2       0                'sec'};
-Pstate.param{3} = {'stim_time'  'float'     1       0                'sec'};
+Pstate.param{end+1} = {'postdelay'  'float'     2       0                'sec'};
+Pstate.param{end+1} = {'stim_time'  'float'     1       0                'sec'};
 
-Pstate.param{4} = {'pulse_dur'       'int'      100       0                'ms'};
-Pstate.param{5} = {'pulse_fr'       'float'      1       0                'Hz'};
-Pstate.param{6} = {'indic_stim'      'int'      1       0                'binary'};
-Pstate.param{7} = {'background'      'float'   0       0                ''};
+Pstate.param{end+1} = {'pulse_ch'  'int'      2       0                ''};
+Pstate.param{end+1} = {'pulse_dur'       'int'      100       0                'ms'};
+Pstate.param{end+1} = {'pulse_fr'       'float'      1       0                'Hz'};
+Pstate.param{end+1} = {'indic_stim'      'int'      1       0                'binary'};
+Pstate.param{end+1} = {'background'      'float'   0       0                ''};
 
 
 
